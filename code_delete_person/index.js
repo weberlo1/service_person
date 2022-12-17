136fcd6aad82bc2cadd352ab77ee5169ffea9ab1
Andrey <kholkinandrey@gmail.com>
2022-12-17 22:02:36 +0100

    test workflow

--- code_delete_person/index.js
+++ code_delete_person/index.js
@@ -1,14 +1,14 @@
 const { Pool } = require('pg')
 
-const pool = new Pool({
-  user: process.env.RDS_USER,
-  host: process.env.RDS_HOSTNAME,
-  database: process.env.RDS_DB,
-  password: process.env.RDS_PW,
-  port: process.env.RDS_PORT,
-})
-
 exports.handler = async (request, context, callback) => {
+  const pool = new Pool({
+    user: process.env.RDS_USER,
+    host: process.env.RDS_HOSTNAME,
+    database: process.env.RDS_DB,
+    password: process.env.RDS_PW,
+    port: process.env.RDS_PORT,
+  })
+
   const client = await pool.connect()
   const { input } = request.arguments
 
@@ -21,21 +21,20 @@ exports.handler = async (request, context, callback) => {
       WHERE persons.id = $1 AND persons.workspace_id = $2
       RETURNING id, workspace_id   
   `,
-      [ input.id, input.workspace_id ]
+      [input.id, input.workspace_id]
     )
 
     if (res.rowCount === 0) {
-        callback(null, {
-            code: 400,
-            message: 'person doesnt exist',
-        })
-    }
-    else {
-        callback(null, {
-            code: 200,
-            message: 'person deleted',
-            data: res.rows[0],
-        })
+      callback(null, {
+        code: 400,
+        message: 'person doesnt exist',
+      })
+    } else {
+      callback(null, {
+        code: 200,
+        message: 'person deleted',
+        data: res.rows[0],
+      })
     }
   } catch (e) {
     callback(e, {
