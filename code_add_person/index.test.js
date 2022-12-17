f0842ea7253114853dc64b8136904347bce36b98
Andrey <kholkinandrey@gmail.com>
2022-12-17 10:20:46 +0100

    test workflow

--- code_add_person/index.test.js
+++ code_add_person/index.test.js
@@ -1,9 +1,7 @@
 const { handler } = require('./index')
 const { Pool } = require('pg')
 const { uuidv4 } = require('uuid')
-  
-  uuidv1(); // -> '6c84fb90-12c4-11e1-840d-7b25c5ee775a' 
-  uuidv4(); // -> '110ec58a-a0f2-4ac4-8393-c866d813b8d1' 
+
 describe('lambda function', () => {
   process.env.RDS_USER = 'postgres'
   process.env.RDS_HOSTNAME = '127.0.0.1'
